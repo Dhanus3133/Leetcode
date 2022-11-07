86fd42da5e924ab78a2cb696518aa110dbfc1240
Dhanus SL <dhanus3133@gmail.com>
2022-11-07 09:00:09 +0530

    Time: 69 ms (17.26%), Space: 9.6 MB (8.10%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.go
@@ -0,0 +1,19 @@
+func groupAnagrams(strs []string) [][]string {
+    m := make(map[string][]string)
+	for _, s := range strs {
+		temp := strings.Split(s, "")
+		sort.Strings(temp)
+		anagram := strings.Join(temp, "")
+		if arr, pres := m[anagram]; pres {
+			m[anagram] = append(arr, s)
+		} else {
+			m[anagram] = []string{s}
+		}
+	}
+	res := [][]string{}
+    for _, v := range m {
+        res = append(res, v)
+    }
+	return res
+
+}
