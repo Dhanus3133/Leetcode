032912d8f6d46c8c934b688ff4418970993d5548
Dhanus SL <dhanus3133@gmail.com>
2022-11-07 13:08:15 +0530

    Time: 33 ms (82.25%), Space: 7.5 MB (88.05%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -0,0 +1,16 @@
+func productExceptSelf(nums []int) []int {
+	pre, post := 1, 1
+	res := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		res[i] = pre
+		pre *= nums[i]
+	}
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		res[i] *= post
+		post *= nums[i]
+	}
+
+	return res
+}
