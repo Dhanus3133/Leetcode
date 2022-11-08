146a40a4487e5695274773e8821b31ba73b287cd
Dhanus SL <dhanus3133@gmail.com>
2022-11-08 19:04:55 +0530

    Time: 154 ms (62.78%), Space: 9.4 MB (86.32%)

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.go
@@ -2,21 +2,15 @@ func longestConsecutive(nums []int) int {
 	if len(nums) == 0 || len(nums) == 1 {
 		return len(nums)
 	}
-  m := make(map[int]bool)
-  fNums := []int{}
-  for _, num := range nums {
-    if _, pres := m[num]; !pres {
-      m[num] = true
-      fNums = append(fNums, num)
-    }
-  }
-  nums = fNums
-  fNums = nil
 	sort.Ints(nums)
 	highest := 1
 	tempHighest := 1
+  prev := nums[0]
 	for i := 1; i < len(nums); i++ {
-		if nums[i-1] == nums[i]-1 {
+    if (prev == nums[i]) {
+      continue
+    }
+		if prev == nums[i]-1 {
 			tempHighest++
 			if tempHighest > highest {
 				highest = tempHighest
@@ -24,6 +18,7 @@ func longestConsecutive(nums []int) int {
 		} else {
 			tempHighest = 1
 		}
+    prev = nums[i]
 	}
 	return highest
 }
